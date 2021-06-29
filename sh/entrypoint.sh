8819990764dd4d6b70e3641cef4017081bf22a90
maikokitchon <maiko.kitchon@gmail.com>
2021-06-29 22:10:36 +0800

    Updates on docker entrypoint script

--- sh/entrypoint.sh
+++ sh/entrypoint.sh
@@ -4,7 +4,7 @@
 WORKDIR="$(dirname "${PWD}")"
 
 # Define scripts that will run in cron
-(crontab -l 2>/dev/null; echo "* * * * * /bin/bash -C ${WORKDIR}/sh/update_repository.sh")|crontab
+(crontab -l 2>/dev/null; echo "* * * * * /bin/bash -C ${WORKDIR}/sh/update_repository.sh > /var/log/cron.log 2>&1")|crontab
 
 # Execute Pybot
 python3 ${WORKDIR}/run.py
--- sh/update_repository.sh
+++ sh/update_repository.sh
@@ -1,13 +1,15 @@
 # !/bin/bash
 
 # Define branch to pull
-BRANCH='develop'
+BRANCH='master'
 
 # Check if the branch that is being monitored is ahead
-IS_AHEAD=$(git status -uno | grep 'Your branch is up to date with')
-echo "${IS_AHEAD}"
-if [ ! -z "${IS_AHEAD}" ]
+IS_AHEAD=$(git fetch --dry-run 2>&1)
+if [ -z "${IS_AHEAD}" ]
 then
+    # No updates. Do nothing here.
+    exit 0
+else
     echo "[INFO] Updates detected. Pulling new changes..."
     git pull origin "${BRANCH}"
 fi
