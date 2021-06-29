e3144384b454c014bd6e26d2a3493b49a8ae1ab3
maikokitchon <maiko.kitchon@gmail.com>
2021-06-29 23:09:21 +0800

    Updates on docker entrypoint script

--- sh/entrypoint.sh
+++ sh/entrypoint.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
 # Define work directory
-WORKDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+SHDIR="${0%/*}"
+WORKDIR=$(dirname "${SHDIR}")
 
 # Define scripts that will run in cron
-(crontab -l 2>/dev/null; echo "* * * * * /bin/bash -C ${WORKDIR}/sh/update_repository.sh > /var/log/cron.log 2>&1")|crontab
+(crontab -l 2>/dev/null; echo "* * * * * /bin/bash -C ${SHDIR}/update_repository.sh > /var/log/cron.log 2>&1")|crontab
 
 # Execute Pybot
-python3 ${WORKDIR}/run.py &
+python3 "${WORKDIR}"/run.py
 
 exec "$@"
