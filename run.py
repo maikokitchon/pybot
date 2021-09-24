138e51cd46f75e9cd349da689e9bcb5f0b5e8fc7
Maiko Kieth Isaac Kitchon <maiko.kitchon@gmail.com>
2021-09-25 02:18:38 +0800

    Update to debug client in action method

--- run.py
+++ run.py
@@ -38,7 +38,7 @@ def catch_event(body, say, logger):
     "action_id": "submitbtn1"
 })
 def update_message(ack, body, client, respond):
-    client.replyInteractive('test message')
+    #client.replyInteractive('test message')
     respond('test')
     #print(body)
     ack()
