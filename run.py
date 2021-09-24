8d88734b0eb7dcbdb8bb162cd7cbd20ee44c7a5b
Maiko Kieth Isaac Kitchon <maiko.kitchon@gmail.com>
2021-09-24 15:37:02 +0800

    Update botkit forms and test submission

--- run.py
+++ run.py
@@ -35,10 +35,11 @@ def catch_event(body, say, logger):
 # Your function will only be called when the action_id matches 'select_user' AND the block_id matches 'assign_ticket'
 @app.action({
     "block_id": "actionblock1",
+	"action_id": "datepickeraction1",    
     "action_id": "submitbtn1"
 })
 def update_message(ack, body, client):
-    print(body['container'])
+    print(body)
     ack()
 
 if __name__ == "__main__":
